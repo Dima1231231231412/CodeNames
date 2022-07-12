673911ea35d28f44c74c8535aae70352ae8dbedf
Dima1231231231412 <donskikh.16@mail.ru>
2022-07-12 20:19:15 +0300

    Готовая игра без авторизации

--- src/main/java/com/example/codenames/Avtorization.java
+++ src/main/java/com/example/codenames/Avtorization.java
@@ -1,7 +1,5 @@
 package com.example.codenames;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+
 import com.google.gson.Gson;
+
 import static java.lang.Math.round;
 
 
@@ -31,11 +31,10 @@ public class Avtorization {
     }
 
     @GetMapping("/cards")
-    public void cards(HttpServletResponse response) throws JSONException, IOException {
+    public void cards(HttpServletResponse response) throws IOException {
         FileReader reader = new FileReader("D:\\Java Projects\\CodeNames\\src\\main\\java\\com\\example\\codenames\\dict.txt");
         int c;
         StringBuilder str = new StringBuilder();
-        JSONObject sampleObject = new JSONObject();
 
         while((c=reader.read())!=-1){
             str.append((char) c);
@@ -46,37 +45,30 @@ public class Avtorization {
         int max = words.length;
         List<String> selectionWords = new ArrayList<>();
         List<String> colors = new ArrayList<>();
-        String colorString = "rbna";
-        String colorStr = "";
+        String str2 = "rrrrrrrrrbbbbbbbbnnnnnnna";
+
+        List<String> letters = Arrays.asList(str2.split(""));
+        Collections.shuffle(letters);
+        String shuffled = "";
+
+        for (String letter : letters) {
+            shuffled += letter;
+        }
+
         for (int i=0; i < 25; i++)
         {
             int rndWord = round((int) (Math.random()*max));
             selectionWords.add(words[rndWord]);
-            int rndColor = round((int) (Math.random() * 4));
-            colors.add(String.valueOf(colorString.charAt(rndColor)));
-            colorStr += colorString.charAt(rndColor);
+
+            colors.add(String.valueOf(shuffled.charAt(i)));
         }
-
-        int countA=0, countB = 0, countR = 0, countN = 0;
-        for (char element : colorStr.toCharArray()){
-            if (element == 'a') countA++;
-            if (element == 'b') countB++;
-            if (element == 'r') countR++;
-            if (element == 'n') countN++;
-        }
-        System.out.println(colorStr);
-
-        System.out.println(countA);
-        System.out.println(countB);
-        System.out.println(countR);
-        System.out.println(countN);
-
 
         Card card = new Card(selectionWords,colors);
         String json = new Gson().toJson(card);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(json);
 
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.getWriter().write(json);
     }
 }
